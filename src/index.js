00af60afc467eb0296b53b10f5ba8f21c3a9ce85
Timur Kalimullin <timur_kalimullin@list.ru>
2020-03-03 02:34:20 +0500

    feat: resolve morse decoder task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,24 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+
+
+function decode(str) {
+    str = str.match(/.{1,10}/g)
+    .map(el=>el.replace(/^0+/g, ''))
+    .map(el=>el.match(/.{1,2}/g))
+    .map(el=>el.map(item=>item=='**'?' ':item=='10'?'.':'-'))
+    .map(el=>el.join(''));
+
+
+
+    for (let i in str) {
+    for (let key in MORSE_TABLE) {
+        str[i]==key?str[i]=MORSE_TABLE[key]:null;
+    }
+    }
+
+   return (str.join('').replace(/ +/g, ' '))
 }
 
 module.exports = {
